4568f065d27c71c68810d36377607d8f9b828242
Michel Demazure <michel@demazure.com>
2015-10-05 10:48:36 +0200

    3.4.0

--- lib/j2r/qt/version.rb
+++ lib/j2r/qt/version.rb
@@ -11,9 +11,9 @@ module JacintheReports
   # major version number
   MAJOR = 3
   # minor version number
-  MINOR = 3
+  MINOR = 4
   # tiny version number
-  TINY = '1a'
+  TINY = '0'
   # version
   VERSION = [MAJOR, MINOR, TINY].join('.')
 
